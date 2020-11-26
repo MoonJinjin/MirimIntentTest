594b5d20b1a8eea9b593661f3746ba72490cde57
MoonJinjin <s2019s11@e-mirim.hs.kr>
2020-11-26 19:40:26 +0900

    Intent

--- app/src/main/java/kr/hs/emirim/s2019s11/mirimintenttest/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/s2019s11/mirimintenttest/MainActivity.java
@@ -8,11 +8,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
     EditText edit1, edit2;
     Button btnGo;
-    RadioButton radioPlus, radioMin, radioMulti, radioDiv;
+    RadioGroup radio;
 
     String num1, num2;
     Integer result;
@@ -23,10 +24,7 @@ public class MainActivity extends AppCompatActivity {
         edit1 = findViewById(R.id.edit_num1);
         edit2 = findViewById(R.id.edit_num2);
         btnGo = findViewById(R.id.btn_go);
-        radioPlus = findViewById(R.id.radio_plus);
-        radioMin = findViewById(R.id.radio_min);
-        radioMulti = findViewById(R.id.radio_multi);
-        radioDiv = findViewById(R.id.radio_div);
+        radio = findViewById(R.id.radio);
 
         btnGo.setOnClickListener(btnListener);
     }
@@ -38,6 +36,25 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
             intent.putExtra("num1", num1);
             intent.putExtra("num2", num2);
+
+            String op = "";
+
+            switch (radio.getCheckedRadioButtonId()) {
+                case R.id.radio_plus:
+                    op = "+";
+                    break;
+                case R.id.radio_min:
+                    op = "-";
+                    break;
+                case R.id.radio_multi:
+                    op = "*";
+                    break;
+                case R.id.radio_div:
+                    op = "/";
+                    break;
+            }
+
+            intent.putExtra("op", op);
             startActivity(intent);
         }
     };
